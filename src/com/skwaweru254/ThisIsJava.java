b0d5481b28be7c45d196d3658c2774e01527eb40
skwaweru254 <skwaweru254@localhost.localdomain>
2023-03-10 10:47:35 +0300

    Initial commit

---
+++ src/com/skwaweru254/ThisIsJava.java
@@ -0,0 +1,19 @@
+package com.skwaweru254;
+
+class ThisIsJava{
+    String name;
+    float salary;
+    ThisIsJava(String name,float salary){
+        this.name=name;
+        this.salary=salary;
+        /*distinguish local variable and instance variable by using this keyword*/
+    }
+    void display(){System.out.println(name+" earns "+salary);}
+}
+class TestThis1{
+    public static void main(String[] args){
+        ThisIsJava s1=new ThisIsJava("Simon",90000);
+        ThisIsJava s2=new ThisIsJava("Waweru",50000);
+        s1.display();
+        s2.display();
+    }}
